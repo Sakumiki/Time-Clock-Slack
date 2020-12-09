b60e7d68c45058485fbbe2cbb7364eda40cc0410
sakumiki <itowdjg3104@gmail.com>
2020-12-09 14:52:55 +0900

    docs(server.js): add dialog.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const app = require('polka');
+const dialog = require(`./dialog`);
 const {
     PORT = 3000
 } = process.env;
 
-app().listen(PORT, err => {
-    if (err) throw err;
-    console.log(`> Running on localhost:${PORT}`);
-});
+app()
+    .use(`/`, dialog),
+    .listen(PORT, err => {
+        if (err) throw err;
+        console.log(`> Running on localhost:${PORT}`);
+    });
